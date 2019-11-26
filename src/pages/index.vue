83d7aa7a0cf572d4337b7bbcd8fd732a6ad744dd
CYP <cypdgg@163.com>
2019-11-26 22:00:57 +0800

    代码初始化

---
+++ src/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="_page">
+    <div class="page-index _container">
+      <div class="index-container">
+        <div class="index-list">
+          <GoodsItem/>
+          <GoodsItem/>
+          <GoodsItem/>
+        </div>
+      </div>
+    </div>
+    <BackTop/>
+    <Foot selectedLabelDefault="商 品"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+  .index-container{
+    margin-top: 7.5px;
+    padding: 0 12.5px;
+  }
+</style>
